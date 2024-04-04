97510a4b7d279889295f1f59418f242e5d628928
Konstantin Sinchilo <SinchiloKon@sbsit.com>
2024-04-04 21:40:14 +0300

    init spm

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ let package = Package(
             name: "TickTimeYandexKit",
             targets: ["TickTimeYandexKit"]),
     ],
+    dependencies: [
+//        .package(url: "https://github.com/yandexmobile/yandexmapkit-ios.git", from: "1.0.0"),
+        .package(url: "https://github.com/yandexmobile/yandexmapkit-ios.git", from: "1.0.10"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
